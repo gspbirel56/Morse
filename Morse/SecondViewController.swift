1e9435ae0d12b5d683e5d2aeefa2d7feff886952
Chris Morris <gspbirel56@gmail.com>
2018-05-08 08:17:07 -0400

    Initial Commit

---
+++ Morse/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Morse
+//
+//  Created by Chris Shula on 5/8/18.
+//  Copyright © 2018 Chris Morris. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
